5a7dfb9ca15a59d60ead48646bc3387329782930
Arjun Kalburgi <askalburgi@gmail.com>
2020-05-04 18:06:07 -0400

    pure js rewrite

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+const containers = document.getElementsByClassName('animation_leaves');
+for (let item of containers) {
+    item.className += " BackgroundAnimation";
+    createAnimations(item);
+}
 
+function createAnimation(container) {
+  const num_of_sprites = parseInt(container.getAttribute('data-number'));
+  var w = container.innerWidth(), h = container.innerHeight();
+
+  for (var i = 0; i < num_of_sprites; i++) {
+	// make icon element
+	var icon = document.createElement('div');
+	icon.className = "BackgroundAnimation_icon";
+
+	// apply animation
+	iconPlace(icon);
+	iconRotate(icon);
+	iconSway(icon);
+
+	// append icon 
+	container.append(icon);
+  }
+}
+
+/*
 // Leaves animation (https://codepen.io/askalburgi/pen/WWjmRM)
 var icons = $('.bgicon'), total = $('.bgicon').length;
 var container = $(".bgicons"), w = container.innerWidth(), h = container.innerHeight();
@@ -10,9 +35,14 @@ for (var i = 0; i < total; i++) {
     iconRotate(icon);
     iconSway(icon);
 }
+*/
 
 function R(min, max) { return min + (Math.floor(10 * Math.random() * (max - min)) / 10) };
 function S() { return Math.random() < 0.5 ? -1 : 1 };
+function iconPlace(icon) {
+    // place icon 
+    TweenLite.set(icon, { x: R(0, w), y: R(0, h), z: R(-200, 200) });
+}
 function iconRotate(icon) {
     // make icon rotate 
     TweenMax.to(icon, R(2, 5), {
